b2e1815a26d7668fb9642d66bef0972bd47f9306
noreebia <noreebia@gmail.com>
2018-12-05 20:09:23 +0900

    Extract balance list so that it becomes outermost element

--- src/WalletInfo.js
+++ src/WalletInfo.js
@@ -4,7 +4,7 @@ import { connect } from 'react-redux';
 
 const mapStateToProps = (state) => {
     return {
-        balances: state.balanceReducer.balances
+        balances: state.balanceReducer
     }
 }
 
--- src/reducers/balance-reducer.js
+++ src/reducers/balance-reducer.js
@@ -1,24 +1,22 @@
 import { SET_BALANCES, REFRESH_BALANCE } from "../actions/types";
 
-const initialState = {
-    balances: [{
-        "currencyName": "ethereum",
-        "currencySymbol": "eth",
-        "balance": 0
-    },
-    {
-        "currencyName": "konkukcoin",
-        "currencySymbol": "kkc",
-        "balance": 0
-    }]
-};
+const initialState = [{
+    "currencyName": "ethereum",
+    "currencySymbol": "eth",
+    "balance": 0
+},
+{
+    "currencyName": "konkukcoin",
+    "currencySymbol": "kkc",
+    "balance": 0
+}];
 
 export default (state = initialState, action) => {
     switch (action.type) {
         case SET_BALANCES:
-            return Object.assign({}, state, { balances: [...action.payload] })
+            return [...action.payload]
         case REFRESH_BALANCE:
-            return initialState;
+            return initialState
         default:
             return state
     }
