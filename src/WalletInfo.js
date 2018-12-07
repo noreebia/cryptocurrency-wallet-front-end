0d57d53367fd7a05294d21e71f100d63dd426026
noreebia <noreebia@gmail.com>
2018-12-08 01:16:43 +0900

    Implement websocket connection

--- src/WalletInfo.js
+++ src/WalletInfo.js
@@ -1,21 +1,47 @@
 import React, { Component } from 'react';
 import CurrencyInfo from "./CurrencyInfo";
 import { connect } from 'react-redux';
+import SockJsClient from 'react-stomp';
+import { setBalances } from './actions/actions';
 
 const mapStateToProps = (state) => {
     return {
+        username: state.reducer.username,
         balances: state.balanceReducer
     }
 }
 
+const mapDispatchToProps = dispatch => {
+    return {
+      setBalances: (balances) => dispatch(setBalances(balances))
+    }
+  }
+
 class WalletInfo extends Component {
+    constructor(props) {
+        super(props);
+        this.receiveDepositNotification = this.receiveDepositNotification.bind(this);
+    }
+
+    receiveDepositNotification(msg){
+        console.log(msg);
+        const { username, balances} = msg;
+        if(username == this.props.username){
+            this.props.setBalances(balances);
+            alert(`You have just received a deposit! You balances have been updated`);
+        }
+    }
 
     render() {
-        const listOfCurrencies = this.props.balances.map((balance, index) => <CurrencyInfo key={index} name={balance.currencyName} symbol={balance.currencySymbol} balance = {balance.balance} />)
+        const listOfCurrencies = this.props.balances.map((balance, index) => <CurrencyInfo key={index} name={balance.currencyName} symbol={balance.currencySymbol} balance={balance.balance} />)
         return (
-            [listOfCurrencies]
+            <div>
+                {listOfCurrencies}
+                <SockJsClient url='http://localhost:8080/websocket' topics={['/topic/deposits']} onConnect={() => { console.log("connected to server!") }} onMessage={this.receiveDepositNotification}
+                   onDisconnect={()=>{console.log("disconnected!");}} ref={(client) => { this.clientRef = client }} />
+            </div>
         )
     }
 }
 
-export default connect(mapStateToProps)(WalletInfo);
+export default connect(mapStateToProps, mapDispatchToProps)(WalletInfo);
